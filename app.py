0e9ce64b2df894285b6514889ee982a98f7e8b69
Mateusz Kacpura <mateusz.kacpura@protonmail.com>
2024-07-24 23:37:18 +0200

    Aktualizacja

--- app.py
+++ app.py
@@ -260,6 +260,28 @@ def definition_pop_up():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/modals/image-pop-up', methods=['GET'])
+def image_pop_up():
+    # print(os.path.abspath('templates/learning/modals/pop-up.html'))
+    # Pobierz parametry modalnego okienka
+    selectedWord = request.args.get('selectedWord')
+    correctWord = request.args.get('correctWord')
+    theme = request.args.get('theme', 'light')
+    modalHeaderClass = 'bg-success text-white' if selectedWord == correctWord else 'bg-danger text-white'
+    modalTitle = 'Poprawna odpowiedź!' if selectedWord == correctWord else 'Nieprawidłowa odpowiedź!'
+    modalMessage = modalTitle
+    
+    # Renderuj szablon modalnego okienka
+    modal_html = render_template(
+        'learning/modals/image-pop-up.html',
+        modalHeaderClass=modalHeaderClass,
+        modalTitle=modalTitle,
+        modalMessage=modalMessage,
+        correctWord=correctWord,
+        theme=theme
+    )
+    return jsonify({'modal_html': modal_html})
+
 @app.route('/modals/insert-pop-up', methods=['GET'])
 def insert_pop_up():
     selectedWord = request.args.get('selectedWord')
